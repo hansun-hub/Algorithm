9b74a195378a245912d2183dd426067a9d9ee03b
김한선 <68306239+hansun-hub@users.noreply.github.com>
2023-09-19 19:25:24 +0900

    Time: 52 ms (80.43%), Space: 17.3 MB (85.43%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,17 +1,29 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        vowels = "AEIOUaeiou"  # 대소문자 모음 문자열
-
-        # 문자열을 리스트로 변환하여 뒤집을 모음 문자들을 저장할 리스트를 생성
+        # 문자열을 리스트로 변환하여 작업하기 편하게 만듭니다.
         s = list(s)
-        vowel_list = [c for c in s if c in vowels][::-1]
-
-        # 문자열을 순회하면서 모음을 뒤집은 리스트의 값으로 교체
-        vowel_idx = 0
-        for i in range(len(s)):
-            if s[i] in vowels:
-                s[i] = vowel_list[vowel_idx]
-                vowel_idx += 1
-
-        # 리스트를 문자열로 변환하여 반환
+        
+        # 모음을 저장할 리스트를 만듭니다.
+        vowels = 'aeiouAEIOU'
+        
+        # 시작 인덱스와 끝 인덱스를 초기화합니다.
+        start, end = 0, len(s) - 1
+        
+        while start < end:
+            # 시작 인덱스에서 모음을 찾을 때까지 전진합니다.
+            while start < end and s[start] not in vowels:
+                start += 1
+            
+            # 끝 인덱스에서 모음을 찾을 때까지 후진합니다.
+            while start < end and s[end] not in vowels:
+                end -= 1
+            
+            # 모음을 서로 교환합니다.
+            s[start], s[end] = s[end], s[start]
+            
+            # 시작과 끝 인덱스를 각각 이동합니다.
+            start += 1
+            end -= 1
+        
+        # 리스트를 다시 문자열로 변환하여 반환합니다.
         return ''.join(s)
