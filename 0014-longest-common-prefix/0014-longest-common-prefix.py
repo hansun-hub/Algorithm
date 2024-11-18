57ddbe55d4e35f58ff27ac9e73550c3c199e4580
김한선 <68306239+hansun-hub@users.noreply.github.com>
2024-11-19 07:27:19 +0900

    Time: 0 ms (100%), Space: 16.6 MB (87.3%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,8 +3,10 @@ class Solution:
         if not strs:
             return ''
         minStr = min(strs,key=len)
-        for i,x in enumerate(minStr):
+        for i, x in enumerate(minStr):
             for other in strs:
-                if other[i] != x :
+                if other[i] != x:
                     return minStr[:i]
         return minStr
+            
+    
